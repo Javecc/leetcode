912238664efa3fc9acbac010edda69b12acfe769
Javecc <1420755674@qq.com>
2018-11-18 16:29:41 +0800

    new md file

--- python_算法教程/01_abc.py
+++ python_算法教程/01_abc.py
@@ -1,5 +1,11 @@
 #if a+b+c=1000,and a^2+b^2=c^2,put out a,b,c
-class Solution:
-	def 
-if __name__ == '__main__':
-	main()
+import time
+
+start_time = time.time()
+for a in range(0,1001):
+	for b in range(0,1001):
+			c = 1000 - a - b
+			if a**2 + b**2 == c**2:
+				print("a = {},b = {},c = {}".format(a,b,c))
+end_time = time.time()
+print("time = {}".format(end_time-start_time))
