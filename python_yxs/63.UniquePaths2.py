ef9dd9843ba487becddd17c37157bc28ef673c13
Javecc <1420755674@qq.com>
2018-11-23 00:52:51 +0800

    change 63.py

--- python_yxs/63.UniquePaths2.py
+++ python_yxs/63.UniquePaths2.py
@@ -13,25 +13,39 @@ class Solution:
             for i in range(row):
                 if obstacleGrid[i][0] == 1:
                     return 0
+        print(obstacleGrid,"first")
+
         #进行0-1转换
         for i in range(row):
             for j in range(column):
                 if obstacleGrid[i][j]==0:
                     obstacleGrid[i][j] = 1
                 else:
-                    obstacleGrid[i][j] = 0
+                    obstacleGrid[i][j] = -1
+        print(obstacleGrid,"0-1置换")
+
+        #尾巴置0
+        for i in range(row-1):
+            for j in range(column-1):
+                if obstacleGrid[i][j] == -1:
+                    obstacleGrid[i][j+1] = 0
+                    obstacleGrid[i+1][j] = 0
+        print(obstacleGrid,"尾部置0")
+
         for i in range(1,row):
             for j in range(1,column):
-                if obstacleGrid[i][j] == 0:
-                    continue
+                if obstacleGrid[i][j] == -1:
+                    obstacleGrid[i][j] = 0
                 else:
                     obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+        print(obstacleGrid,"end")
         return obstacleGrid[-1][-1]
 #2018.11.22
 #------------------main function-----------------
 if __name__ == '__main__':
     # ls = [[0,0,0],[0,1,0],[0,0,0]]
-    ls = [[0,0],[1,1],[0,0]]
+    # ls = [[0,0],[1,1],[0,0]]
+    ls = [[0,0],[1,0]]
     s = Solution()
     nums = s.uniquePathsWithObstacles(ls)
     print(nums)
