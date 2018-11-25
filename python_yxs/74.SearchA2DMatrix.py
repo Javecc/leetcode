f41e42abc623782a9f43252e50b1a360250d032a
javecc <1420755674@qq.com>
2018-11-25 22:18:55 +0800

    2018.11.25 add 74.py

--- python_yxs/74.SearchA2DMatrix.py
+++ python_yxs/74.SearchA2DMatrix.py
@@ -5,4 +5,33 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        
+        templist = []
+        row = len(matrix)
+        if row == 0:
+            return False
+        column = len(matrix[0])
+        if column == 0:
+            return False
+        for i in range(row):
+            for j in range(column):
+                templist.append(matrix[i][j])
+        # print(templist)
+        fir = 0
+        end = len(templist)-1
+        while(fir <= end):
+            mid = (fir + end)//2
+            if templist[mid] == target:
+                return True
+            elif templist[mid] > target:
+                end = mid-1
+            else:
+                fir = mid+1
+        return False
+#2018.11.25
+#-----------------main function-------------------
+if __name__ == '__main__':
+    ls = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+    target = 13
+    s = Solution()
+    print(s.searchMatrix(ls,target))
+    
