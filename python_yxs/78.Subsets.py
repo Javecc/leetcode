9c4c5ebc7923a7f4f754c9cd5278c1869566f111
javecc <1420755674@qq.com>
2018-11-27 22:45:49 +0800

    2018.11.27 add 78.py

--- python_yxs/78.Subsets.py
+++ python_yxs/78.Subsets.py
@@ -4,4 +4,24 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        rlist = [[]]
+        for num in nums:
+            for temp in rlist[:]:
+                x = temp[:]
+                # x = temp
+                x.append(num)
+                rlist.append(x)
+        return rlist
+#2018.11.27
+#---------------main function-----------------
+if __name__ == '__main__':
+    ls = [1,2,3] 
+    s = Solution()
+    l = s.subsets(ls)
+    print(l)
+# l = [1,2,3]
+# ls = l[:]
+# print(id(l),id(l[:]))
+# ls.append(4)
+# print(l)
+# print(ls)
