67ec4ea0bbb7cd1cbfc9b85094886c42d1faa13a
javecc <1420755674@qq.com>
2018-12-04 19:10:52 +0800

    2018.12.04 add 119.py

--- python_yxs/119.Pascal'sTriangle.py
+++ python_yxs/119.Pascal'sTriangle.py
@@ -4,4 +4,15 @@ class Solution:
         :type rowIndex: int
         :rtype: List[int]
         """
+        p = [1]
+        if not rowIndex:
+            return p
+        for j in range(rowIndex):
+            p = [1] + [p[i]+p[i+1] for i in range(len(p)-1)] + [1]
+        return p
 #2018.12.04
+#------------------main function--------------------
+if __name__ == '__main__':
+    num = 32
+    s = Solution()
+    print(s.getRow(num))
