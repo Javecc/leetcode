ec6c13b3c38ea63a1173a498e34006591594a65b
javecc <1420755674@qq.com>
2018-12-06 22:52:02 +0800

    2018.12.06 add 122.py

--- python_yxs/122.BestTimeToBuyAndSellStock2.py
+++ python_yxs/122.BestTimeToBuyAndSellStock2.py
@@ -4,4 +4,17 @@ class Solution:
         :type prices: List[int]
         :rtype: int
         """
+        length = len(prices)
+        maxpro,tmp = 0,0
+        for i in range(1,length):
+            tmp = prices[i]-prices[i-1]
+            if tmp > 0:
+                maxpro += tmp
+        return maxpro
 #2018.12.06
+#-------------------main function---------------------
+if __name__ == '__main__':
+    # l = [7,1,5,3,6,4]
+    l = [2,1,4,2,1,5]
+    s = Solution()
+    print(s.maxProfit(l))
