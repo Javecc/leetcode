c5db1bb86dda1e32b600420a7aad0c291c61d299
javecc <1420755674@qq.com>
2018-12-11 18:33:52 +0800

    2018.12.11 add 283.py

--- python_yxs/283.MoveZeroes.py
+++ python_yxs/283.MoveZeroes.py
@@ -4,6 +4,16 @@ class Solution:
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        length = len(nums)
+        k = 0
+        if length == 0:
+            return
+        for i in range(length):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+        for i in range(k,length):
+            nums[i] = 0
 #2018.12.11
 #------------------main function--------------------
 if __name__ == '__main__':
