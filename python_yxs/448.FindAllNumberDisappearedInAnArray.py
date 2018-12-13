7f026cbe94e934a6e56185e3f0eada643f51abef
javecc <1420755674@qq.com>
2018-12-13 21:43:47 +0800

    2018.12.13 add 448.py

---
+++ python_yxs/448.FindAllNumberDisappearedInAnArray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        rlist = [i+1 for i in range(len(nums))]
+        for i in range()
+        return rlist
+#2018.12.13
+#-----------------main function-----------------
+if __name__ == '__main__':
+    l = [4,3,2,7,8,2,3,1]
+    s = Solution()
+    print(s.findDisappearedNumbers(l))
