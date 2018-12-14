08c11bacbe013eefd3406882cd7b9cab784611d5
javecc <1420755674@qq.com>
2018-12-14 22:33:50 +0800

    2018.12.14 add 628.py

---
+++ python_yxs/628.MaximumProductOfThreeNumbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        if nums[-1]
+        return nums[-1]*nums[-2]*nums[-3]
+#2018.12.14
+#---------------main function----------------
+if __name__ == '__main__':
+    l = [1,2,3,4]
+    s = Solution()
+    print(s.maximumProduct(l))
