807fd37886dfd46e9f671c76940cd28d8fd16e8f
javecc <1420755674@qq.com>
2018-12-17 22:01:05 +0800

    2018.12.15 add 485.py

--- python_yxs/485.MaxConsecutiveOnes.py
+++ python_yxs/485.MaxConsecutiveOnes.py
@@ -4,4 +4,19 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        maxnums = 0
+        x = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                maxnums = max(maxnums,x)
+                x = 0
+            else:
+                x += 1
+        maxnums = max(maxnums,x)
+        return maxnums
 #2018.12.15
+#----------------main function-----------------
+if __name__ == '__main__':
+    l = [1,1,0,1,1,1]
+    s = Solution()
+    print(s.findMaxConsecutiveOnes(l))
